7a075f0209969c96b45a68eedab0e4e1d30423bc
HuppyRedTeam <chen1222345678@sina.com>
2019-01-04 12:31:10 +0800

    新建错误类

---
+++ Prop_Loader/PropFile_Exception.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prop_Loader
+{
+    class PropFile_Exception : ApplicationException
+    {
+        private string error_message;
+        public PropFile_Exception(string message)
+        {
+            this.error_message = message;
+
+        }
+
+        public override string ToString()
+        {
+            return error_message;
+        }
+
+    }
+
+}
